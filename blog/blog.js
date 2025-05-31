4e91e96c264cecda641c0c9d56d692684e87fd63
jschaefer18 <joshschaefer@gmail.com>
2025-05-31 12:49:28 -0600

    blog part 2 done

--- blog/blog.js
+++ blog/blog.js
@@ -17,11 +17,45 @@ const articles = [
     date: 'December 12, 2021',
     description:
       'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
-    imgSrc:
-      'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+    imgSrc: 'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
     imgAlt: 'Book cover for Magnus Chase 1',
     ages: '12–16',
     genre: 'Fantasy',
     stars: '⭐⭐⭐⭐'
+  },
+  {
+    id: 3,
+    title: 'Belgariad Book One: Pawn of Prophecy',
+    date: 'Feb 12, 2022',
+    description:
+      "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+    imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg',
+    imgAlt: 'Book cover for Pawn of Prophecy',
+    ages: '12-16',
+    genre: 'Fantasy',
+    stars: '⭐⭐⭐⭐⭐'
   }
 ]
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('article-list')
+
+  articles.forEach(article => {
+    const articleEl = document.createElement('article')
+    articleEl.innerHTML = `
+      <div class="meta">
+        <p>${article.date}</p>
+        <p>${article.ages}</p>
+        <p>${article.genre}</p>
+        <p>${article.stars}</p>
+      </div>
+      <div class="content">
+        <h3>${article.title}</h3>
+        <img src="${article.imgSrc}" alt="${article.imgAlt}" />
+        <p>${article.description} <a href="#">Read More...</a></p>
+      </div>
+    `
+    container.appendChild(articleEl)
+  })
+})
+
